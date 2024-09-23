6d29ff5f27ca1de740bb0e7cc923eb90fedabf64
Agustin Genoves <agustingenoves@hotmail..com>
2024-09-23 10:37:05 -0300

    feat: Cambios en la forma de generar fechas
    Ahora se generan las fechas en base a una fecha inicial y una final.

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,9 @@
-from datetime import datetime, timedelta
+import datetime
 
 
 def generate_dates_until_today(
-    start_date: datetime = datetime(2017, 11, 26)
+    start_date: datetime.date = datetime.date(2017, 11, 26),
+    end_date: datetime.date = datetime.date.today() - datetime.timedelta(days=1),
 ) -> list[str]:
     """
     Genera una lista de fechas en el formato YYYYMMDD desde el 1 de enero del año dado hasta hoy.
@@ -10,20 +11,19 @@ def generate_dates_until_today(
     :param start_year: El año desde el cual empezar a generar fechas.
     :return: Una lista de strings con fechas en el formato YYYYMMDD.
     """
-    end_date = datetime.now() - timedelta(days=1)
-    if start_date.date() > end_date.date():
+    if start_date > end_date:
         raise ValueError(
             "La fecha de inicio no puede ser mayor o igual a la fecha actual."
         )
 
-    if start_date < datetime(2017, 11, 26):
-        start_date = datetime(2017, 11, 26)
+    if start_date < datetime.date(2017, 11, 26):
+        start_date = datetime.date(2017, 11, 26)
 
     dates = []
     current_date = start_date
 
-    while current_date.date() <= end_date.date():
+    while current_date <= end_date:
         dates.append(current_date.strftime("%Y%m%d"))
-        current_date += timedelta(days=1)
+        current_date += datetime.timedelta(days=1)
 
     return dates
