ea15b0bb89eb958f1b737c859f0f86a8cb488fda
Agustin Ignacio Genoves <agustingenoves@hotmail.com>
2024-09-25 09:05:42 -0300

    Revert "feat: Mejoras en la iterfaz de descarga"
    This reverts commit ca76a6cd56635f1251f2d428da9d46cc4d8cd810.

--- download.py
+++ download.py
@@ -10,6 +10,9 @@ from src.utils import generate_dates_until_today
 from src import parser
 
 
+st.title("Descargar datos diarios desde el SMN")
+
+
 main_path = Path(__file__).resolve()
 main_directory = main_path.parent
 relative_path = main_directory / "data"
@@ -17,15 +20,23 @@ relative_path = main_directory / "data"
 min_date = datetime.date(2017, 11, 26)
 max_date = datetime.date.today() - datetime.timedelta(days=1)
 
+start_date, end_date = st.date_input(
+    "Selecciona una fecha",
+    value=(min_date, max_date),
+    min_value=min_date,
+    max_value=max_date,
+    format="DD/MM/YYYY",
+)
 
-def download(start_date: datetime.date, end_date: datetime.date, out_file_name: str):
+ok = st.button("Descargar información")
+
+if ok:
     st.write(
         "Inicio:",
         start_date.strftime("%d/%m/%Y"),
         "fin:",
         end_date.strftime("%d/%m/%Y"),
     )
-
     dates_list = generate_dates_until_today(start_date, end_date)
 
     last_five_downloads = []
@@ -53,7 +64,7 @@ def download(start_date: datetime.date, end_date: datetime.date, out_file_name: 
 
     st.success("¡Todos los archivos han sido descargados!")
 
-    output_parquet_file = relative_path / output_file_name
+    output_parquet_file = relative_path / "all_smn_daily_data.parquet"
     previus_data = pd.DataFrame()
     if os.path.exists(output_parquet_file):
         previus_data = pd.read_parquet(output_parquet_file, engine="pyarrow")
@@ -65,24 +76,3 @@ def download(start_date: datetime.date, end_date: datetime.date, out_file_name: 
 
         previus_data = pd.concat([previus_data, datahorario_smn], ignore_index=True)
     previus_data.to_parquet(output_parquet_file, engine="pyarrow")
-
-
-st.title("Descargar datos diarios desde el SMN")
-
-
-output_file_name = st.text_input("Nombre del arhivo de salida:")
-
-start_date, end_date = st.date_input(
-    "Seleccione un rango de fechas",
-    value=(min_date, max_date),
-    min_value=min_date,
-    max_value=max_date,
-    format="DD/MM/YYYY",
-)
-
-
-if st.button("Descargar información"):
-    if output_file_name and start_date and end_date:
-        download(start_date, end_date, output_file_name)
-    else:
-        st.error("Por favor, ingresa los campos antes de continuar")
